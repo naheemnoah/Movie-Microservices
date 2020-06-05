8461f3afe4af324c4e8df8836076bae90f27d9f6
Naheem Noah <54889953+Naheem001@users.noreply.github.com>
2020-06-05 14:44:29 +0100

    Adding Discovery Server(Eureka Client) Springboot App and Eureka Service in all services

--- movie-catalog-service/src/main/java/com/naijamovies/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/naijamovies/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,13 @@ package com.naijamovies.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	//create a Rest Template method that spring uses throughout
--- movie-info-service/src/main/java/com/naijamovies/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/naijamovies/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,11 +2,10 @@ package com.naijamovies.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoServiceApplication {
 	
 

eb08d6c6390ae87c2f999246224167d6e1ed9358
Naheem Noah <54889953+Naheem001@users.noreply.github.com>
2020-06-08 16:28:28 +0100

    Implementing Hystrix with Fall Back method

--- movie-catalog-service/src/main/java/com/naijamovies/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/naijamovies/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,15 +2,16 @@ package com.naijamovies.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogServiceApplication {
 
 	//create a Rest Template method that spring uses throughout
--- movie-catalog-service/src/main/java/com/naijamovies/moviecatalogservice/RestController/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/com/naijamovies/moviecatalogservice/RestController/MovieCatalogResources.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +12,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.naijamovies.moviecatalogservice.Entity.CatalogItem;
-import com.naijamovies.moviecatalogservice.Entity.Movie;
 import com.naijamovies.moviecatalogservice.Entity.MovieSummary;
 import com.naijamovies.moviecatalogservice.Entity.UserRating;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/catalog")
